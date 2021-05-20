86e62c47356fa4b871ea04e08b3d5c0b5f52b086
deli-hub <deli8908@gmail.com>
2021-05-20 18:34:00 +0900

    methods/error handle

--- learningGo/accounts/accounts.go
+++ learningGo/accounts/accounts.go
@@ -1,5 +1,7 @@
 package accounts
 
+import "errors"
+
 // Account struct
 type Account struct {
 	// 변수명이 대문자로 시작할 경우는 export가 가능해서 public / 소문자면 private
@@ -7,9 +9,34 @@ type Account struct {
 	balance int
 }
 
+var errNoMoney = errors.New("can't withdraw you are poor")
+
 // NewAccount creates Account (How to make a contructor)
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	// 주소값을 복사해서 내보낸다. (반환값이 복사본이 아닌 객체임)
 	return &account
+}
+
+// Deposit x amount on your account
+/* func (a Account) Deposit(amount int) {
+   여기서 받아오는 receiver Account는 struct의 복사본이다.
+   즉, 복사본의 balance만 바꿔줬기 때문에 원본에서는 적용이 되지 않는다. */
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+// Balance of your account
+func (a Account) Balance() int {
+	return a.balance
+}
+
+// Withdraw x amount from your amount
+// errors handling
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
 }
--- learningGo/main.go
+++ learningGo/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/deli-hub/learningGo/accounts"
 )
@@ -10,5 +11,12 @@ func main() {
 	account := accounts.NewAccount("merry")
 	// 아래 코드는 작동하지 않는다. balance가 private기 때문!
 	// account.balance = 10000
-	fmt.Println(account)
+	account.Deposit(1000)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(1500)
+	if err != nil {
+		// 로그를 출력하고 프로그램을 종료한다.
+		log.Fatalln(err)
+	}
+	fmt.Println(account.Balance())
 }
