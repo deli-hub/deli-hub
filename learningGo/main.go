34982b810521b661f0279987ea0cdc89cdb7a753
deli-hub <deli8908@gmail.com>
2021-05-20 18:45:28 +0900

    auto-called method

--- learningGo/main.go
+++ learningGo/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/deli-hub/learningGo/accounts"
 )
@@ -12,11 +11,12 @@ func main() {
 	// 아래 코드는 작동하지 않는다. balance가 private기 때문!
 	// account.balance = 10000
 	account.Deposit(1000)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(1500)
-	if err != nil {
-		// 로그를 출력하고 프로그램을 종료한다.
-		log.Fatalln(err)
-	}
-	fmt.Println(account.Balance())
+	fmt.Println(account)
+	// err := account.Withdraw(1500)
+	// if err != nil {
+	// 	// 로그를 출력하고 프로그램을 종료한다.
+	// 	// log.Fatalln(err)
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(account.Balance(), account.Owner())
 }
