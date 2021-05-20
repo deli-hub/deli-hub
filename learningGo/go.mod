8024588520564e556d8f9fe774ad342f519bc0aa
deli-hub <deli8908@gmail.com>
2021-05-20 16:04:27 +0900

    separate folder

---
+++ learningGo/go.mod
@@ -0,0 +1,3 @@
+module github.com/deli-hub/learningGo
+
+go 1.16
---
+++ learningGo/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "std/github.com/deli-hub/learningGo/banking"
+
+func main() {
+	account := banking.Account {
+		Owner : "merry",
+		Balance : 1000
+	}
+}
